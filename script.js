59a88200176ac69e95d94a4b49f4d5da67bb4714
Svetlana Khaydoshkina <sveta13131313@yandex.ru>
2021-08-02 21:26:28 +0300

    Задание выполнено

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict'
+/* ДЗ 2
 let num = 266219; 
 let res = 1;
 
@@ -14,3 +16,51 @@ res=res.toString();
 console.log(typeof res);
 console.log('Первая цифра ',res[0]);
 console.log('Вторая цифра ',res[1]);
+*/
+
+//ДЗ 3
+let lang = prompt('Введите язык ru или en?');
+//a
+if (lang == 'ru') {
+    alert('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+} else {
+if (lang =='en') {
+    alert('Monday, tuesday, wednesday, thursday, friday, saturday, sunday');
+} else {
+    alert('Вы сделали неправильный ввод, повторите попытку');
+}
+}
+
+
+//b
+let lang1 = prompt('Введите язык ru или en?');
+switch (lang1) {
+    case ('ru'):
+        alert('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+        break;
+    case ('en'):
+        alert('Monday, tuesday, wednesday, thursday, friday, saturday, sunday');
+        break;
+    default:
+        alert('Вы сделали неправильный ввод, повторите попытку');
+}
+
+
+//c
+let lang2=[
+    ['Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'],
+    ['Monday, tuesday, wednesday, thursday, friday, saturday, sunday'],
+    ['Вы сделали неправильный ввод, повторите попытку']
+]
+let choose = prompt('Введите язык ru или en?');
+(choose=='ru')?alert(lang2[0]):
+(choose=='en')?alert(lang2[1]):
+alert(lang2[2]);
+
+
+//2
+let namePerson = prompt('Введите имя');
+
+(namePerson=='Артем')?console.log('Директор'):
+(namePerson=='Максим')?console.log('Преподаватель'):
+console.log('Студент');
